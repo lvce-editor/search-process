2deba473e7e38cb496f1d472ed8141d541ef14c2
Le Vivilet <levivlet@pm.me>
2024-08-10 08:33:45 +0200

    feature: remove unused code

--- src/parts/Logger/Logger.js
+++ src/parts/Logger/Logger.js
@@ -25,22 +25,10 @@ const getOrCreateLogger = () => {
   return state.console
 }
 
-export const log = (...args) => {
-  const logger = getOrCreateLogger()
-  logger.log(...args)
-  console.log(...args)
-}
-
 export const info = (...args) => {
   const logger = getOrCreateLogger()
   logger.info(...args)
   console.info(...args)
-}
-
-export const warn = (...args) => {
-  const logger = getOrCreateLogger()
-  logger.warn(...args)
-  console.warn(...args)
 }
 
 export const error = (...args) => {
