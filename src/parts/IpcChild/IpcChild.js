a7582c1c0b555cabe9db5db966396a867838e39e
Le Vivilet <levivlet@pm.me>
2024-12-04 19:42:58 +0100

    fix: use module create instead of module.listen

--- src/parts/IpcChild/IpcChild.js
+++ src/parts/IpcChild/IpcChild.js
@@ -1,8 +1,8 @@
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.js'
 
 export const listen = async ({ method, ...params }) => {
-  const module = await IpcChildModule.getModule(method)
+  const module = IpcChildModule.getModule(method)
   // @ts-ignore
-  const rpc = await module.listen(params)
+  const rpc = await module.create(params)
   return rpc
 }
