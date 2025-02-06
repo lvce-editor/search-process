b6934fa95b1320d58f5b8d4c9ae41326e5d845cf
Le Vivilet <levivlet@pm.me>
2025-02-06 21:51:07 +0100

    build

--- packages/build/src/bundleJs.js
+++ packages/build/src/bundleJs.js
@@ -9,7 +9,7 @@ import { root } from './root.js'
  * @type {import('rollup').RollupOptions}
  */
 const options = {
-  input: join(root, 'packages/preview-process/src/previewProcessMain.ts'),
+  input: join(root, 'packages/search-process/src/searchProcessMain.ts'),
   preserveEntrySignatures: 'strict',
   treeshake: {
     propertyReadSideEffects: false,
