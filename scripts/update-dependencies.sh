2224196a8539bcd8182084ffe9fa3fe194cfd3cb
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-02-06 22:12:03 +0100

    feature: remove unused code (#47)
    * feature: remove unused code
    
    * package
    
    * lint
    
    * lint
    
    * lint
    
    * dependencies
    
    * update
    
    * test
    
    * build

--- scripts/update-dependencies.sh
+++ scripts/update-dependencies.sh
@@ -16,7 +16,7 @@ fi
 
 function updateDependencies {
   echo "updating dependencies..."
-  OUTPUT=`ncu -u -x @types/node -x rollup`
+  OUTPUT=`ncu -u -x @types/node`
   SUB='All dependencies match the latest package versions'
   if [[ "$OUTPUT" == *"$SUB"* ]]; then
     echo "$OUTPUT"
@@ -26,7 +26,9 @@ function updateDependencies {
   fi
 }
 
-updateDependencies &&
+                                                       updateDependencies             &&
+cd packages/build                                   && updateDependencies && cd ../.. &&
+cd packages/search-process                          && updateDependencies && cd ../.. &&
 
 echo "Great Success!"
 
