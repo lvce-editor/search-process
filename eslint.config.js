ed1d012bcbbb6a9435c8b169984e2f6b4472c008
Le Vivilet <levivlet@pm.me>
2025-02-07 19:36:42 +0100

    lint

--- eslint.config.js
+++ eslint.config.js
@@ -4,22 +4,12 @@ export default [
   ...config.default,
   ...config.recommendedNode,
   {
-    ignores: [
-      'scripts',
-      '**/mockCdpImport.js',
-      'rollup.config.js',
-      'eslint.config.js',
-      'packages/preview-process/src/previewProcessMain.ts',
-      'packages/preview-process/src/parts/WaitForServerToBeReady/WaitForServerToBeReady.ts',
-      'packages/preview-process/files/previewInjectedCode.js',
-      'packages/build',
-    ],
+    ignores: ['scripts', 'rollup.config.js', 'eslint.config.js', 'packages/build'],
   },
   {
     files: ['**/*.ts'],
     rules: {
-      'n/no-unsupported-features/node-builtins': 'off',
-      'n/no-unsupported-features/es-syntax': 'off',
+      // 'n/no-unsupported-features/node-builtins': 'off',
       '@typescript-eslint/no-unused-vars': 'off',
       'no-console': 'off',
     },
