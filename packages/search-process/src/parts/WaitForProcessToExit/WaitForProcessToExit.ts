d8882ef470a9885e9ec8b7a453d44ce6d3f9a814
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-04-05 10:46:00 +0200

    feature: add exitEvent type (#67)

--- packages/search-process/src/parts/WaitForProcessToExit/WaitForProcessToExit.ts
+++ packages/search-process/src/parts/WaitForProcessToExit/WaitForProcessToExit.ts
@@ -1,8 +1,9 @@
 import type { BaseChildProcess } from '../BaseChildProcess/BaseChildProcess.ts'
+import type { ExitEvent } from '../ExitEvent/ExitEvent.ts'
 import * as GetFirstEvent from '../GetFirstEvent/GetFirstEvent.ts'
 import * as ProcessExitEventType from '../ProcessExitEventType/ProcessExitEventType.ts'
 
-export const waitForProcessToExit = (childProcess: BaseChildProcess): Promise<any> => {
+export const waitForProcessToExit = (childProcess: BaseChildProcess): Promise<ExitEvent> => {
   return GetFirstEvent.getFirstEvent(childProcess, {
     error: ProcessExitEventType.Error,
     close: ProcessExitEventType.Exit,
