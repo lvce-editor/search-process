3624b708868aa4faf95fc4e243326cb5717743ed
Le Vivilet <levivlet@pm.me>
2025-04-05 13:49:59 +0200

    update max memory

--- packages/memory/src/config.ts
+++ packages/memory/src/config.ts
@@ -1,6 +1,6 @@
 import { join } from 'node:path'
 import { root } from './root.ts'
 
-export const instantiations = 4500
+export const instantiations = 12_000
 
 export const instantiationsPath = join(root, 'packages', 'search-process')
