040afc4909c8d5965dd1ce4f7a85a63468813173
Le Vivilet <levivlet@pm.me>
2025-04-05 13:58:27 +0200

    feature: remove ts ignore

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -2,7 +2,6 @@ import { measureMemory } from '@lvce-editor/measure-memory'
 import { instantiations, instantiationsPath } from './config.ts'
 
 const main = async () => {
-  // @ts-ignore
   await measureMemory({
     instantiations,
     instantiationsPath,
