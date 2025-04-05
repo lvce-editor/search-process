5e43092b37bf9f5f3a1431b9392d41411c8e87c4
Le Vivilet <levivlet@pm.me>
2025-04-05 13:59:10 +0200

    undo

--- packages/memory/src/measureMemory.ts
+++ packages/memory/src/measureMemory.ts
@@ -2,6 +2,7 @@ import { measureMemory } from '@lvce-editor/measure-memory'
 import { instantiations, instantiationsPath } from './config.ts'
 
 const main = async () => {
+  // @ts-ignore
   await measureMemory({
     instantiations,
     instantiationsPath,
