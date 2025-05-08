bd389726a0eba662d2c4f46f1f31278d28c001b5
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-08 22:17:36 +0200

    feature: simplify ipc create (#126)
    * feature: simplify ipc create
    
    * add type

--- packages/search-process/src/parts/IpcChild/IpcChild.ts
+++ packages/search-process/src/parts/IpcChild/IpcChild.ts
@@ -1,7 +1,8 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
-export const listen = async ({ method, ...params }: { readonly method: number; readonly [key: string]: any }): Promise<any> => {
-  const module = IpcChildModule.getModule(method)
-  const rpc = await module.create(params)
+export const listen = async ({ method, ...params }: { readonly method: number; readonly [key: string]: any }): Promise<Rpc> => {
+  const create = IpcChildModule.getModule(method)
+  const rpc = await create(params)
   return rpc
 }
