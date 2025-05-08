9b1a9177318bce984231a27d907ea17a0d61f407
Le Vivilet <levivlet@pm.me>
2025-05-08 22:15:05 +0200

    add type

--- packages/search-process/src/parts/IpcChild/IpcChild.ts
+++ packages/search-process/src/parts/IpcChild/IpcChild.ts
@@ -1,6 +1,7 @@
+import type { Rpc } from '@lvce-editor/rpc'
 import * as IpcChildModule from '../IpcChildModule/IpcChildModule.ts'
 
-export const listen = async ({ method, ...params }: { readonly method: number; readonly [key: string]: any }): Promise<any> => {
+export const listen = async ({ method, ...params }: { readonly method: number; readonly [key: string]: any }): Promise<Rpc> => {
   const create = IpcChildModule.getModule(method)
   const rpc = await create(params)
   return rpc
