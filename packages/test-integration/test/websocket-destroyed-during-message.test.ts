de6677bfe908be60105e11838a42e75e15f84b8a
Le Vivilet <levivlet@pm.me>
2025-05-18 22:42:28 +0200

    feature: add socket destroy test

--- packages/test-integration/test/websocket-destroyed-during-message.test.ts
+++ packages/test-integration/test/websocket-destroyed-during-message.test.ts
@@ -1,15 +1,7 @@
-import { expect, test } from '@jest/globals'
+import { test } from '@jest/globals'
 import getPort from 'get-port'
 import { createServer } from 'node:http'
-import {
-  createWebSocket,
-  getHandleMessage,
-  setup,
-  startServer,
-  waitForRequest,
-  waitForResponse,
-  waitForWebSocketToBeOpen,
-} from '../src/setup.ts'
+import { createWebSocket, getHandleMessage, setup, startServer, waitForRequest, waitForWebSocketToBeOpen } from '../src/setup.ts'
 
 test('search process handles websocket connection and search commands', async () => {
   const { rpc, addDisposable, testDir, setFiles } = await setup()
@@ -40,9 +32,6 @@ test('search process handles websocket connection and search commands', async ()
       externalSocket.close()
     },
   })
-
-  const responsePromise = waitForResponse(externalSocket)
-
   externalSocket.send(
     JSON.stringify({
       jsonrpc: '2.0',
@@ -57,8 +46,10 @@ test('search process handles websocket connection and search commands', async ()
       ],
     }),
   )
-  externalSocket.close()
-
-  // const response = await responsePromise
-  // expect(response).toEqual({ jsonrpc: '2.0', id: 111, result: 'index.ts' })
+  // @ts-ignore
+  const s = externalSocket._socket
+  s.destroy()
+  await new Promise((r) => {
+    setTimeout(r, 100)
+  })
 }, 20_000)
