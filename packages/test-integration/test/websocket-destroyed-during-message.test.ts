b7e5802e7480aef515c439ed06b89d4b90a39c43
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-18 22:33:18 +0200

    feature: add websocket destroyed during message e2e test (#142)

---
+++ packages/test-integration/test/websocket-destroyed-during-message.test.ts
@@ -0,0 +1,64 @@
+import { expect, test } from '@jest/globals'
+import getPort from 'get-port'
+import { createServer } from 'node:http'
+import {
+  createWebSocket,
+  getHandleMessage,
+  setup,
+  startServer,
+  waitForRequest,
+  waitForResponse,
+  waitForWebSocketToBeOpen,
+} from '../src/setup.ts'
+
+test('search process handles websocket connection and search commands', async () => {
+  const { rpc, addDisposable, testDir, setFiles } = await setup()
+  await setFiles({
+    'index.ts': 'let x = 1',
+  })
+  const server = createServer()
+
+  addDisposable({
+    async dispose() {
+      const { resolve, promise } = Promise.withResolvers()
+      server.close(resolve)
+      await promise
+    },
+  })
+
+  const port = await getPort()
+  const requestPromise = waitForRequest(server)
+  await startServer(server, port)
+  const externalSocket = createWebSocket(port)
+  const openPromise = waitForWebSocketToBeOpen(externalSocket)
+  const { request, socket } = await requestPromise
+  const handleMessage = getHandleMessage(request)
+  await rpc.invokeAndTransfer('HandleWebSocket.handleWebSocket', socket, handleMessage)
+  await openPromise
+  addDisposable({
+    async dispose() {
+      externalSocket.close()
+    },
+  })
+
+  const responsePromise = waitForResponse(externalSocket)
+
+  externalSocket.send(
+    JSON.stringify({
+      jsonrpc: '2.0',
+      id: 111,
+      method: 'SearchFile.searchFile',
+      params: [
+        {
+          searchPath: testDir,
+          limit: 100,
+          ripGrepArgs: ['--files', '--sort-files'],
+        },
+      ],
+    }),
+  )
+  externalSocket.close()
+
+  // const response = await responsePromise
+  // expect(response).toEqual({ jsonrpc: '2.0', id: 111, result: 'index.ts' })
+}, 20_000)
