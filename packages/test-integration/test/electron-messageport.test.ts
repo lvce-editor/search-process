34435cc5547b183d7c223c76e71e06538ef2d7d8
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-19 19:37:20 +0200

    fix: electron path on windows (#149)

--- packages/test-integration/test/electron-messageport.test.ts
+++ packages/test-integration/test/electron-messageport.test.ts
@@ -2,14 +2,23 @@ import { expect, test } from '@jest/globals'
 import { spawn } from 'node:child_process'
 import { join } from 'node:path'
 import { setup } from '../src/setup.ts'
+import { createRequire } from 'node:module'
 
-test.skip('search process works with electron messageport', async () => {
+test('search process works with electron messageport', async () => {
+  if (process.platform === 'linux') {
+    // TODO
+    return
+  }
   const { testDir, setFiles, root } = await setup()
   await setFiles({
     'index.ts': 'let x = 1',
   })
   const fixturePath = join(root, 'packages', 'test-integration', 'fixtures', 'electron-message-port')
-  const child = spawn('electron', ['--no-sandbox', fixturePath, `--test-dir`, testDir], {
+  const require = createRequire(import.meta.url)
+  const electronModulePath = require.resolve('electron')
+  const electronModule = await import(electronModulePath)
+  const electronPath = electronModule.default
+  const child = spawn(electronPath, ['--no-sandbox', fixturePath, `--test-dir`, testDir], {
     stdio: 'inherit',
   })
   const { resolve, promise } = Promise.withResolvers<number>()
