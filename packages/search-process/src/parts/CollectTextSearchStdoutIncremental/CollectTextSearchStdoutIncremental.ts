2614252a51dd16466f6b8d04eed72c667eab3dc6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-20 19:54:35 +0200

    feature: add incremental text search (#154)

---
+++ packages/search-process/src/parts/CollectTextSearchStdoutIncremental/CollectTextSearchStdoutIncremental.ts
@@ -0,0 +1,115 @@
+import { Writable } from 'node:stream'
+import { pipeline } from 'node:stream/promises'
+import type { BaseChildProcess } from '../BaseChildProcess/BaseChildProcess.ts'
+import type { IncrementalSearch } from '../IncrementalSearch/IncrementalSearch.ts'
+import type { IncremetalStdoutResult } from '../IncrementalStdoutResult/IncrementalStdoutResult.ts'
+import * as EncodingType from '../EncodingType/EncodingType.ts'
+import * as GetNewLineIndex from '../GetNewLineIndex/GetNewLineIndex.ts'
+import * as IncrementalSearchState from '../IncrementalSearchState/IncrementalSearchState.ts'
+import * as RipGrepParsedLineType from '../RipGrepParsedLineType/RipGrepParsedLineType.ts'
+import * as TextSearchResultType from '../TextSearchResultType/TextSearchResultType.ts'
+import * as ToTextSearchResult from '../ToTextSearchResult/ToTextSearchResult.ts'
+
+export const collectStdoutIncremental = async (
+  id: string,
+  childProcess: BaseChildProcess,
+  maxSearchResults: number,
+  charsBefore: number,
+  charsAfter: number,
+): Promise<IncremetalStdoutResult> => {
+  const allSearchResults = Object.create(null)
+  let buffer = ''
+  let stats = {}
+  let limitHit = false
+  let numberOfResults = 0
+
+  const search: IncrementalSearch = {
+    dispose() {
+      childProcess.kill()
+    },
+    getResultCount() {
+      return numberOfResults
+    },
+    getItems() {
+      return []
+    },
+  }
+
+  IncrementalSearchState.set(id, search)
+
+  childProcess.stdout.setEncoding(EncodingType.Utf8)
+
+  const handleLine = (line: string): void => {
+    const parsedLine = JSON.parse(line)
+    const { type, data } = parsedLine
+    switch (type) {
+      case RipGrepParsedLineType.Begin:
+        allSearchResults[data.path.text] = [
+          {
+            type: TextSearchResultType.File,
+            start: 0,
+            end: 0,
+            lineNumber: 0,
+            text: data.path.text,
+          },
+        ]
+        break
+      case RipGrepParsedLineType.Match:
+        const remaining = maxSearchResults - numberOfResults
+        const matches = ToTextSearchResult.toTextSearchResult(parsedLine, remaining, charsBefore, charsAfter)
+        numberOfResults += matches.length
+        allSearchResults[data.path.text].push(...matches)
+        break
+      case RipGrepParsedLineType.Summary:
+        stats = data
+        break
+      default:
+        break
+    }
+  }
+
+  const handleData = (chunk: any): void => {
+    let newLineIndex = GetNewLineIndex.getNewLineIndex(chunk)
+    const dataString = buffer + chunk
+    if (newLineIndex === -1) {
+      buffer = dataString
+      return
+    }
+    newLineIndex += buffer.length
+    let previousIndex = 0
+    while (newLineIndex >= 0) {
+      const line = dataString.slice(previousIndex, newLineIndex)
+      handleLine(line)
+      previousIndex = newLineIndex + 1
+      newLineIndex = GetNewLineIndex.getNewLineIndex(dataString, previousIndex)
+    }
+    buffer = dataString.slice(previousIndex)
+
+    if (numberOfResults > maxSearchResults) {
+      limitHit = true
+      childProcess.kill()
+    }
+  }
+
+  await pipeline(
+    childProcess.stdout,
+    new Writable({
+      decodeStrings: false,
+      construct(callback): void {
+        callback()
+      },
+      write(chunk, encoding, callback): void {
+        try {
+          handleData(chunk)
+          callback()
+        } catch (error) {
+          callback(error)
+        }
+      },
+    }),
+  )
+  return {
+    stats,
+    limitHit,
+  }
+}
