b221d8637f43729f13cb7fe6da0f86acbdc7aea6
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-20 21:11:46 +0200

    feature: register incremental text search command (#155)

--- packages/search-process/src/parts/CommandMap/CommandMap.ts
+++ packages/search-process/src/parts/CommandMap/CommandMap.ts
@@ -2,10 +2,12 @@ import * as HandleElectronMessagePort from '../HandleElectronMessagePort/HandleE
 import * as HandleWebSocket from '../HandleWebSocket/HandleWebSocket.ts'
 import * as SearchFile from '../SearchFile/SearchFile.ts'
 import * as TextSearch from '../TextSearch/TextSearch.ts'
+import * as TextSearchIncremental from '../TextSearchIncremental/TextSearchIncremental.ts'
 
 export const commandMap = {
   'HandleElectronMessagePort.handleElectronMessagePort': HandleElectronMessagePort.handleElectronMessagePort,
-  'SearchFile.searchFile': SearchFile.searchFile,
   'HandleWebSocket.handleWebSocket': HandleWebSocket.handleWebSocket,
+  'SearchFile.searchFile': SearchFile.searchFile,
   'TextSearch.search': TextSearch.search,
+  'TextSearch.searchIncremental': TextSearchIncremental.textSearchIncremental,
 }
