0086e19403378eb9fa8bc93a417dc0e523d35846
Le Vivilet <levivlet@pm.me>
2025-05-20 23:27:12 +0200

    debug

--- packages/search-process/src/parts/CollectTextSearchStdoutIncremental/CollectTextSearchStdoutIncremental.ts
+++ packages/search-process/src/parts/CollectTextSearchStdoutIncremental/CollectTextSearchStdoutIncremental.ts
@@ -40,6 +40,7 @@ export const collectStdoutIncremental = async (
   IncrementalSearchState.set(id, search)
 
   childProcess.stdout.setEncoding(EncodingType.Utf8)
+  childProcess.stderr.pipe(process.stderr)
 
   const handleLine = (line: string): void => {
     const parsedLine = JSON.parse(line)
