b70b9588bcbfd60a8a552ec3349eb25ef75662c7
Le Vivilet <72156503+levivilet@users.noreply.github.com>
2025-05-20 23:25:29 +0200

    feature: add search state test (#165)
    * feature: add search state test
    
    * lint

---
+++ packages/search-process/test/IncrementalSearchState.test.ts
@@ -0,0 +1,20 @@
+import { test, expect } from '@jest/globals'
+import type { IncrementalSearch } from '../src/parts/IncrementalSearch/IncrementalSearch.ts'
+import { get, set, remove } from '../src/parts/IncrementalSearchState/IncrementalSearchState.js'
+
+const mockSearch: IncrementalSearch = {
+  dispose: () => {},
+  getResultCount: () => {},
+  getItems: () => [],
+}
+
+test('set and get IncrementalSearch', () => {
+  set('abc', mockSearch)
+  expect(get('abc')).toBe(mockSearch)
+})
+
+test('remove IncrementalSearch', () => {
+  set('def', mockSearch)
+  remove('def')
+  expect(get('def')).toBeUndefined()
+})
