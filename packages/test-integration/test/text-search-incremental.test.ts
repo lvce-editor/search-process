ed149513da21e959cb9f23334c2cd9bc327218ce
Le Vivilet <levivlet@pm.me>
2025-05-20 23:08:43 +0200

    feature: enable incremental search test

---
+++ packages/test-integration/test/text-search-incremental.test.ts
@@ -0,0 +1,142 @@
+import { expect, test } from '@jest/globals'
+import getPort from 'get-port'
+import { createServer, IncomingMessage, ServerResponse } from 'node:http'
+import { join } from 'node:path'
+import { root, setup, startServer, waitForRequest } from '../src/setup.ts'
+
+interface StdoutResponse {
+  readonly stdout: string
+}
+
+const getResponse1 = (): StdoutResponse => {
+  return {
+    stdout: JSON.stringify({ type: 'begin', data: { path: { text: './index.ts' } } }),
+  }
+}
+
+const getResponse2 = (): StdoutResponse => {
+  return {
+    stdout: JSON.stringify({
+      type: 'match',
+      data: {
+        path: { text: './index.ts' },
+        lines: { text: 'let x = 1' },
+        line_number: 0,
+        absolute_offset: 4,
+        submatches: [{ match: { text: 'x' }, start: 4, end: 5 }],
+      },
+    }),
+  }
+}
+
+const getResponse3 = (): StdoutResponse => {
+  return {
+    stdout: JSON.stringify({
+      type: 'end',
+      data: {
+        path: { text: './index.ts' },
+        binary_offset: null,
+        stats: {
+          elapsed: { secs: 0, nanos: 105604, human: '0.000106s' },
+          searches: 1,
+          searches_with_match: 1,
+          bytes_searched: 7732,
+          bytes_printed: 4386,
+          matched_lines: 18,
+          matches: 20,
+        },
+      },
+    }),
+  }
+}
+
+const getResponseUnknown = (): StdoutResponse => {
+  return {
+    stdout: '',
+  }
+}
+
+const getResponse = (url: string): StdoutResponse => {
+  switch (url) {
+    case '/1':
+      return getResponse1()
+    case '/2':
+      return getResponse2()
+    case '/3':
+      return getResponse3()
+    default:
+      return getResponseUnknown()
+  }
+}
+
+test('incremental text search', async () => {
+  const fixturePath = join(root, 'packages', 'test-integration', 'fixtures', 'search-incremental', 'ripgrep-incremental.js')
+
+  const port = await getPort()
+  const { testDir, rpc, addDisposable } = await setup({
+    env: {
+      LVCE_SEARCH_PROCESS_PORT: port.toString(),
+      RIP_GREP_PATH: fixturePath,
+    },
+  })
+
+  const server = createServer()
+
+  addDisposable({
+    async dispose() {
+      const { resolve, promise } = Promise.withResolvers()
+      server.close(resolve)
+      await promise
+    },
+  })
+
+  const request1Promise = waitForRequest(server)
+  await startServer(server, port)
+
+  const id = '1'
+  const resultPromise = rpc.invoke('TextSearch.searchIncremental', {
+    id,
+    searchPath: testDir,
+    limit: 100,
+    ripGrepArgs: [
+      '--hidden',
+      '--no-require-git',
+      '--smart-case',
+      '--stats',
+      '--json',
+      '--threads',
+      '1',
+      '--ignore-case',
+      '--fixed-strings',
+      '--',
+      'aa',
+      '.',
+    ],
+  })
+
+  const item1 = await request1Promise
+
+  const request2Promise = waitForRequest(server)
+
+  item1.response.end(JSON.stringify(getResponse(item1.request.url || '')))
+
+  const intermediateResult1 = await rpc.invoke('TextSearch.getIncrementalResults', id)
+
+  expect(intermediateResult1).toEqual([])
+
+  const item2 = await request2Promise
+
+  const request3Promise = waitForRequest(server)
+
+  item2.response.end(JSON.stringify(getResponse(item2.request.url || '')))
+
+  const intermediateResult2 = await rpc.invoke('TextSearch.getIncrementalResults', id)
+
+  expect(intermediateResult2).toEqual([])
+
+  const item3 = await request3Promise
+
+  item3.response.end(JSON.stringify(getResponse(item3.request.url || '')))
+
+  await resultPromise
+})
