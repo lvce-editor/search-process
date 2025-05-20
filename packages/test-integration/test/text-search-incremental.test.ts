1714b5030cd8b06f7f71e9a82786c681492c403f
Le Vivilet <levivlet@pm.me>
2025-05-20 23:41:59 +0200

    windows

--- packages/test-integration/test/text-search-incremental.test.ts
+++ packages/test-integration/test/text-search-incremental.test.ts
@@ -72,6 +72,9 @@ const getResponse = (url: string): StdoutResponse => {
 }
 
 test('incremental text search', async () => {
+  if (process.platform === 'win32') {
+    return
+  }
   const fixturePath = join(root, 'packages', 'test-integration', 'fixtures', 'search-incremental', 'ripgrep-incremental.js')
 
   const port = await getPort()
