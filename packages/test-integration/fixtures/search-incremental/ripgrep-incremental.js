6a6e1278fa48434a0eea78001f3014246bd21307
Le Vivilet <levivlet@pm.me>
2025-05-20 23:33:01 +0200

    debug

--- packages/test-integration/fixtures/search-incremental/ripgrep-incremental.js
+++ packages/test-integration/fixtures/search-incremental/ripgrep-incremental.js
@@ -16,7 +16,9 @@ const getResponse = async (id) => {
 }
 
 const main = async () => {
+  console.error('before request 1')
   const result1 = await getResponse(1)
+  console.error('after request 1')
   process.stdout.write(result1.stdout)
   const result2 = await getResponse(2)
   process.stdout.write(result2.stdout)
