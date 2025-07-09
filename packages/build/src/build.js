d4946ee7a79e1a20a6f33048a2ad0b3fca9540ad
Le Vivilet <levivlet@pm.me>
2025-07-09 10:20:54 +0200

    feature: move ws version into separate file

--- packages/build/src/build.js
+++ packages/build/src/build.js
@@ -3,6 +3,7 @@ import { cp, mkdir, readFile, rm, writeFile } from 'node:fs/promises'
 import { join } from 'node:path'
 import { bundleJs } from './bundleJs.js'
 import { root } from './root.js'
+import { getWsVersion } from './getWsVersion.js'
 
 const dist = join(root, '.tmp', 'dist')
 
@@ -58,18 +59,7 @@ const version = await getVersion()
 
 const packageJson = await readJson(join(root, 'packages', 'search-process', 'package.json'))
 
-const getWsVersion = async () => {
-  const { stdout } = await execa('npm', ['ls', 'ws', '--json'], {
-    cwd: join(root, 'packages', 'search-process'),
-  })
-  const parsed = JSON.parse(stdout)
-  const wsVersion =
-    parsed.dependencies['@lvce-editor/rpc'].dependencies['@lvce-editor/ipc'].dependencies['@lvce-editor/web-socket-server']
-      .dependencies['ws'].version
-  return wsVersion
-}
-
-const wsVersion = await getWsVersion()
+const wsVersion = await getWsVersion(join(root, 'packages', 'search-process'))
 
 delete packageJson.scripts
 delete packageJson.devDependencies
