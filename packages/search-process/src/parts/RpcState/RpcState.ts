b334ef36380dd1bd86cc6834013b54d1282893e4
Le Vivilet <levivilet@pm.me>
2026-07-30 15:05:45 +0000

    feature: add knip

--- packages/search-process/src/parts/RpcState/RpcState.ts
+++ packages/search-process/src/parts/RpcState/RpcState.ts
@@ -7,24 +7,7 @@ interface State {
 const state: State = {
   rpc: undefined,
 }
-const rpcMap = new Map<string, Rpc>()
-
-export const get = (): Rpc | undefined => {
-  return state.rpc
-}
 
 export const set = (value: Rpc): void => {
   state.rpc = value
-}
-
-export const getById = (id: string): Rpc | undefined => {
-  return rpcMap.get(id)
-}
-
-export const setById = (id: string, value: Rpc): void => {
-  rpcMap.set(id, value)
-}
-
-export const removeById = (id: string): void => {
-  rpcMap.delete(id)
 }
